66798458f09ae9f8a0c66872d8a97f2132c062b8
Sergio Castro <sergiojuniorce@gmail.com>
2025-03-04 11:39:37 -0700

    asd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,52 @@
-import { Hono } from 'hono'
-import { prisma } from './lib/db.js'
-import { serve } from '@hono/node-server'
-const app = new Hono()
+import { Hono } from 'hono';
+import { cors } from 'hono/cors'
+import { prisma } from './lib/db.js';
+import { serve } from '@hono/node-server';
+const app = new Hono();
+
+// Add CORS middleware
+app.use('/*', cors({
+  origin: 'http://localhost:5173',
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type'],
+  exposeHeaders: ['Content-Length', 'X-Requested-With'],
+  credentials: true,
+}));
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
 app.get('/todos', async (c) => {
-  const todos = await prisma.todo.findMany()
-  return c.json(todos)
-})
+  const todos = await prisma.todo.findMany();
+  return c.json(todos);
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.post('/todos', async (c) => {
+  const { title, category } = await c.req.json();
+  const todo = await prisma.todo.create({
+    data: { title, category: { connect: { id: category } } },
+  });
+  return c.json(todo);
+});
+
+app.get('/categories', async (c) => {
+  const categories = await prisma.category.findMany();
+  return c.json(categories);
+});
+
+app.post('/categories', async (c) => {
+  const { name } = await c.req.json();
+  const category = await prisma.category.create({ data: { name } });
+  return c.json(category);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,11 +1,17 @@
+import React, { useEffect } from 'react';
+
 import type { Route } from './+types/home';
-import { useEffect } from 'react';
-import { useState } from "react";
+import { useState } from 'react';
 
 type Todo = {
   id: string;
   title: string;
   completed: boolean;
+};
+
+type Category = {
+  id: string;
+  name: string;
 };
 
 export function meta({}: Route.MetaArgs) {
@@ -23,35 +29,99 @@ const fetchTodos = async () => {
 
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
 
+  
   useEffect(() => {
     fetchTodos().then(setTodos);
   }, []);
+
+  useEffect(() => {
+    fetchCategories().then((categories) => {
+      console.log(categories);
+      setCategories(categories);
+    });
+  }, []);
+
+  const fetchCategories = async () => {
+    const response = await fetch('http://localhost:3000/categories');
+    const data = await response.json();
+    return data;
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.target as HTMLFormElement);
     const title = formData.get('title') as string;
+    const category = formData.get('category') as string;
+    fetch('http://localhost:3000/todos', {
+      method: 'POST',
+      body: JSON.stringify({ title, category }),
+    });
+
+    fetchTodos().then(setTodos);
+  };
+
+  const handleSubmitCategory = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    const category = formData.get('category') as string;
+    fetch('http://localhost:3000/categories', {
+      method: 'POST',
+      body: JSON.stringify({ name: category }),
+    });
+
+    fetchCategories().then(setCategories);
   };
 
   return (
-    <div>
-      <h1>Home</h1>
-      {
-        todos.length > 0 ? (
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id}>{todo.title}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No todos found</p>
-        )
-      }
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1 className="text-2xl font-bold">Home</h1>
+      {todos.length > 0 ? (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-2xl font-bold bg-white text-black rounded px-2">
+          No todos found
+        </p>
+      )}
 
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="title" />
-        <button type="submit">Add Todo</button>
+      <form onSubmit={handleSubmitCategory}>
+        <input
+          type="text"
+          name="category"
+          className="rounded px-2 bg-white text-black"
+          placeholder="Add a category"
+        />
+        <button type="submit" className="rounded px-2 bg-white text-black">
+          Add category
+        </button>
+      </form>
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex gap-2 items-center justify-center "
+      >
+        <input
+          type="text"
+          name="title"
+          className="rounded px-2 bg-white text-black"
+          placeholder="Add a todo"
+        />
+        <select name="category" className="rounded px-2 bg-white text-black">
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+        <button type="submit" className="rounded px-2 bg-white text-black">
+          Add Todo
+        </button>
       </form>
     </div>
   );
